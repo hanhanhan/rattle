b03912fe0525b0d9cf0539b72611f6081bf1b03c
Han Lazarus <han.lazarus@gmail.com>
2016-12-07 03:49:40 -0800

    Added basic mouse interaction -- can drag circle very carefully from within radius

--- rattle.js
+++ rattle.js
@@ -13,6 +13,12 @@ let spacing = canvas.width/(length + 2);
 let xInit = canvas.width/(length + 2);
 let yInit = canvas.height/2;
 
+let mouse = {
+    down: false,
+    x: null,
+    y: null
+}
+
 //point object
 class Point {
     constructor(x, y){
@@ -25,14 +31,26 @@ class Point {
         this.yAcceleration = 0;
     }
 
-    update(timeStep){
-        //one path based on mouse interaction - set "prior" xy to mouse coords
-        //otherwise nothing
+    update(){
+        if(mouse.down){
+            let xDiff = mouse.x - this.x;
+            let yDiff = mouse.y - this.y;
+            let distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
+            console.log(distance);
+          
+            if(distance < radius){
+                this.xPrior = this.x;
+                this.yPrior = this.y;
+                this.x = mouse.x;
+                this.y = mouse.y;
+                console.log(distance);
+            }
+        }
         return this;
     }
 
     draw(){
-        context.moveTo(this.x + radius, this.y);
+        //context.moveTo(this.x + radius, this.y);
         context.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
         context.stroke();
     }
@@ -50,8 +68,8 @@ class Points {
             //vs points.push(point) ??
         }
     }
-    draw(){
-        this.points.forEach(point => point.draw())
+    updateAndDraw(){
+        this.points.forEach(point => point.update().draw())
     }
 }
 
@@ -60,10 +78,31 @@ class Points {
 
 //points object
 
+//user interaction
+window.onmousedown = () => mouse.down = true;
+window.onmouseup = () => mouse.down = false;
 
-//initial call
+window.onmousemove = mouseMovement;
+
+function mouseMovement(e){
+    //another way to prevent this event listener from firing if mouse.down = false?
+    //is it worthwhile savings?
+    // if (!mouse.down){ 
+    //     return; 
+    // }
+    mouse.x = e.x - canvas.getBoundingClientRect().left;
+    mouse.y = e.y - canvas.getBoundingClientRect().top;
+}
+
+
+//initialization
 let rattle = new Points();
-//context.beginPath();
-rattle.draw();
-//context.stroke();
-//animation loop
+animationLoop();
+
+function animationLoop(){
+    //context.beginPath();
+    rattle.updateAndDraw();
+    //context.stroke();
+    window.requestAnimationFrame(animationLoop);
+}
+
